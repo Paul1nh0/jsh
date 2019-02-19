ec6a8878ce76ed1d73b3b5e1635762ef96873b3f
akayn <30512781+akayn@users.noreply.github.com>
2019-02-18 19:03:53 -0800

    Update run.bat

--- automation/sc/run.bat
+++ automation/sc/run.bat
@@ -1,3 +1,4 @@
+FIXME!!
 @Echo OFF
 "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\cdb.exe" -g C:\\Users\\akayn\\Desktop\\jssh\\chakra\\cc_windows_1_7_5\\x64_release\\ch.exe C:\Users\akayn\desktop\automation\SC\curpus\11\fuzz-0.js
 timeout /T 60
