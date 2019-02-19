1abc14605844efcdf1a6145efe7b0a6690f2a983
akayn <30512781+akayn@users.noreply.github.com>
2019-02-18 21:49:57 -0800

    Update lEdge.cpp

--- ledge/lEdge.cpp
+++ ledge/lEdge.cpp
@@ -5,17 +5,9 @@
 
 #include "stdafx.h"
 
-wchar_t* charToWChar(char* text)
-{
-	size_t size = strlen(text) + 1;
-	wchar_t* wa = new wchar_t[size];
-	mbstowcs(wa, text, size);
-	return wa;
-}
-
 void main(int argc, char* argv[])
 {
-	//auto lpparam = charToWChar(argv[2]);
+
 	if (argc < 2) {
 		return;
 	}
